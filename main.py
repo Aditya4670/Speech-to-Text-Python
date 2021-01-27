7104c83be9b83e7600928f3d6abcac1ecc50ede4
Aditya4670 <71327576+Aditya4670@users.noreply.github.com>
2021-01-27 16:10:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import speech_recognition as sr
+import pyautogui
+
+recogniser = sr.Recognizer()
+
+
+def order():
+    try:
+        with sr.Microphone() as source:
+            recogniser.adjust_for_ambient_noise(source, duration=1)
+            print('listening...')
+            voice = recogniser.listen(source)
+
+            command = recogniser.recognize_google(voice).lower()
+
+            print(command)
+            return command
+    except:
+        print("Sorry, something went wrong!")
+
+
+while True:
+    query = order()
+    if "quit" in query:
+        quit()
+    else:
+        pyautogui.typewrite(query)
